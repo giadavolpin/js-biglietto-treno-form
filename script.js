26257fbbb6d8169d17dd0d4c46a13b14250f34a8
giadavolpin <volpingiada@gmail.com>
2022-10-10 17:52:34 +0200

    revisione

--- script.js
+++ script.js
@@ -10,41 +10,62 @@ se utente età under 18 prezzo biglietto
 Altrimenti se utente età over65 prezzo biglietto prezzoBiglietto
     prezzoBiglietto * scontoOver65
 */
-const btn = document.getElementById ('calcola').innerHTML;
-const prezzoKm = 0.21
-const scontoUnder18 = 0.20
-const scontoOver65 = 0.40 
 
 
+// salviamo in una variabile il nostro bottone
+const bottone = document.getElementById('bottone');
 
-const calcolaBiglietto = function () {
-    let numeroKm = document.getElementById ('km').value;
-    let age = document.getElementById ('age').value;
-    /* console.log (km);
-    console.log (age); */
-    
-    if(isNaN (numeroKm) || isNaN(etaUtente)) {
-            alert('devi inserire numeri')
-        };
-    let prezzoBiglietto = numeroKm * prezzoKm
-    if(etaUtente <= 18){
-        prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * scontoUnder18)
-        messaggio += ` ${prezzoBiglietto}€ Hai ricevuto uno sconto del 20%`;
-
-    } else if (etaUtente >= 65){
-        prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto + scontoOver65)
-        messaggio += ` ${prezzoBiglietto.toFixed(2)} € Hai ricevuto uno sconto del 40%`;
-    } else {
-        messaggio += ` ${prezzoBiglietto.toFixed(2) } €` 
-    }
-}
-
-/* 
-btn.addEventListener ('click', calcolaBiglietto); */
-
-let messaggio = `
-<h1> Il costo del tuo biglietto è: </h1> `;
+//rimaniamo in ascolto di un evento click su quel bottone
+bottone.addEventListener('click',
+  function () {
+     // prendiamo il valore dell'input km
+     const kmDaPercorrere = document.getElementById('km').value;
+     console.log(kmDaPercorrere);
+      // prendiamo il valore della select fascia eta
+    const fasciaEta = document.getElementById('fascia-eta').value;
+    console.log(fasciaEta);
+     // I nostri dati per il calcolo del biglietto
+     const prezzoKm = 0.21;
+     console.log('prezzo al km', prezzoKm);
+     let costoBiglietto = prezzoKm * kmDaPercorrere;
+     console.log('costo base biglietto', costoBiglietto);
+     //calcoliamo il costo e l'offerta applicata
+    if (fasciaEta == 'minorenne') {
+        // 20% sconto
+        costoBiglietto -= (costoBiglietto * 20 / 100);
+        offerta = 'Sconto Minorenne';
   
+      } else if (fasciaEta == 'over65') {
+        // 40% sconto
+        costoBiglietto -= (costoBiglietto * 40 / 100);
+        offerta = 'Sconto Silver';
+      }
+  
+      // lasciamo solo due decimali
+      costoBiglietto = costoBiglietto.toFixed(2);
 
+       // stampiamo
+    document.getElementById('nome-passeggero').innerHTML = nome;
+    document.getElementById('offerta-applicata').innerHTML = offerta;
+    document.getElementById('costo').innerHTML = costoBiglietto + '€';
+  }
+  );
+// salviamo in una variabile il nostro bottone annulla
+const bottoneAnnulla = document.getElementById('annulla');
 
-document.getElementById('biglietto').innerHTML += messaggio;
+//rimaniamo in ascolto di un evento click sul bottone annulla
+bottoneAnnulla.addEventListener('click',
+  function () {
+    //nascondiamo il biglietto
+    document.getElementById('biglietto').className = 'hidden';
+
+    // svuotiamo gli input
+    document.getElementById('km').value = '';
+    document.getElementById('fascia-eta').value = '';
+
+    // svuotiamo gli altri elementi
+    document.getElementById('nome-passeggero').innerHTML = '';
+    document.getElementById('offerta-applicata').innerHTML = '';
+    document.getElementById('costo').innerHTML = '';
+  }
+);
